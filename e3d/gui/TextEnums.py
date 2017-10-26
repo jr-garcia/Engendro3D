a927f118fb37dac192d2eb6f73a81d5615cb8110
Javier R. García <jr-garcia@users.noreply.github.com>
2017-10-25 22:30:26 -0500

    Add shader values as TextEnums.

---
+++ e3d/gui/TextEnums.py
@@ -0,0 +1,11 @@
+class FontWeightEnum(object):
+    Light = .4
+    Normal = .6
+    Bold = .9
+
+
+class OutlineLenghtEnum(object):
+    NoOutline = 0
+    Little = 0.125
+    Medium = 0.25
+    Big = 0.5
