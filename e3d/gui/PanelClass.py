f14c1f08b65bc3c4e81728b9fce1c31a476d5664
Javier R. García <jr-garcia@users.noreply.github.com>
2017-11-19 00:37:51 -0600

    Add missing style parameter.

--- e3d/gui/PanelClass.py
+++ e3d/gui/PanelClass.py
@@ -10,8 +10,8 @@ class Panel(BaseControl):
     """
 
     def __init__(self, left, top, width, height, parent, pinning=PinningEnum.TopLeft, color=None, ID=None,
-                 imgID=None, rotation=None):
-        style = DefaultStyle(color)
+                 imgID=None, rotation=None, style=None):
+        style = style or DefaultStyle(color)
         if color is None:
             color = style.backgroundColor
         super(Panel, self).__init__(left, top, width, height, parent, pinning, color, ID, imgID, rotation,
